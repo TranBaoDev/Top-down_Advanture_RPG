264c2d03c47fb680aab1ba556b1b9467bf35cd5f
Tran Bao <baotrantq.dev@gmail.com>
2025-12-10 06:21:33 +0700

    Add enemy and player health systems
    Introduced Enemy and Player folders for animations and scripts, added W_Skeleton enemy with movement and combat scripts, and implemented PlayerHealth script with health bar UI.

---
+++ Assets/Scripts/Enemies/Enemy_Movement.cs
@@ -0,0 +1,51 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Transform player;
+
+    private bool isChasing;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isChasing == true)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            rb.linearVelocity = direction * speed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if(player == null)
+            {
+                player = collision.transform;
+            }
+            isChasing = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            rb.linearVelocity = Vector2.zero;
+            isChasing = false;
+        }
+    }
+
+}
+
+
